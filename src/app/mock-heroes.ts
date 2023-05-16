0fb3f71f13b1403944ee60fd51b01db2296fba7d
Kevin Strange <kevin.strange1000@gmail.com>
2023-05-16 14:52:46 +0100

    display a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Captain Quirk' },
+  { id: 13, name: 'The Hilarious Hurricane' },
+  { id: 14, name: 'Paste-Pot Pete' },
+  { id: 15, name: 'Sir Chuckles-a-Lot' },
+  { id: 16, name: 'The Jolly Jester' },
+  { id: 17, name: 'D-Man' },
+  { id: 18, name: 'The Whimsical Wonder' },
+  { id: 19, name: 'The Punny Protector' },
+  { id: 20, name: 'The Giggling Guardian' },
+  { id: 21, name: 'Condiment King' },
+];
